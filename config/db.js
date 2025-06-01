7356a65fcacc5aba4256ebfcd4f2716cd0a21360
Farah El kandouchi <elkandouchifarah@gmail.com>
2025-06-01 13:22:33 +0100

    first test commit

--- config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
- export const connectDB = async () => {
- await mongoose.connect('mongodb+srv://ikramelazhary78:UiX1Uh0my1dl5BpB@cluster0.4u12arw.mongodb.net/backend--pfe').then(()=>console.log("DB Connected"));
-}
